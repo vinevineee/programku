19f4bf0428b3fe733749169aad5a9723bc0eb4bb
vinevineee <vinemade28@gmail.com>
2026-01-19 06:07:34 +0000

    game version 1

---
+++ game.py
@@ -0,0 +1,11 @@
+# Aplikasi game secret number
+
+secret_number = 67
+guess_number = int(input("Masukkan Tebak Angka:"))
+
+while guess number != secret_number:
+    print("Tebak salah, silahkan ciba lagi")
+    guess_number = int(input("Masukkan Tebak Angka:"))
+
+print("Selamat..!!, Tebakan anda benar!!!")
+print("Kode ini saya buat di codespace")
